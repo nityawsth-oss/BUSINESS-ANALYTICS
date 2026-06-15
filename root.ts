6e1e9c841d4645b0c63263ee6814075e93703663
nityawsth-oss <nityawsth@gmail.com>
2026-06-15 12:49:29 +0530

    Update root.ts

--- root.ts
+++ root.ts
@@ -10,7 +10,7 @@ import {
 import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
-import { reportLovableError } from "../lib/lovable-error-reporting";
+import { reportgemma4Error } from "../lib/lovable-error-reporting";
 import { TopNav } from "../components/TopNav";
 
 function NotFoundComponent() {
